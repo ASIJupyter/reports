0c457f00bf9c008e4e53aede311f34a53843adc3
Zhipeng Zhao <zhzhao@microsoft.com>
2019-04-08 14:15:33 -0700

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="Sentinel-Utilities",
-    version="0.1.3",
+    version="0.1.4",
     author="Sentinel Notebooks Devs",
     author_email="zhzhao@microsoft.com",
     description="Tools for Sentinel Notebooks",
